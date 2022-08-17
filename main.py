f18ca8746e745126a1be76a187d39ed3f86b60a7
Felix Grüning <fegrue@gmail.com>
2022-08-17 23:43:20 +0200

    link with ui

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from pytube import YouTube
 from sys import argv
 
 
-link = argv[1]
+link = input("Enter link:")
+
 yt = YouTube(link)
 print("Title: ", yt.title)
 print("Duration: ", yt.length)
 
 yd = yt.streams.get_highest_resolution()
 
-yd.download('./downloaded')
+yd.download('./')
