c08d8ebddf11345a4542878fa4cd6f563c7bd7ca
Felix Grüning <fegrue@gmail.com>
2022-08-21 02:10:43 +0200

    resolution-print

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ print("Title: ", yt.title)
 print("Duration: ", yt.length)
 
 yd = yt.streams.get_highest_resolution()
+for stream in yt.streams:
+    print(stream.resolution)
+
+
 
 yd.download('./')
